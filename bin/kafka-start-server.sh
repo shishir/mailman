dbc4b2a9644a768331d5547a51edf0c3f3a4bfa0
shishir <shishir.das@gmail.com>
2018-04-16 22:34:18 +1000

    vendorize kafka

---
+++ bin/kafka-start-server.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+./vendor/kafka_2.11-1.1.0/bin/kafka-server-start.sh config/server.properties
---
+++ bin/zookeeper-start-server.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+./vendor/kafka_2.11-1.1.0/bin/zookeeper-server-start.sh config/zookeeper.properties
