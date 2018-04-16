35f4fc69ea44a72b613175c72541ca14428e6b07
shishir <shishir.das@gmail.com>
2018-04-16 21:19:18 +1000

    Add status, timestamsp to email

--- db/migrate/1_create_emails.rb
+++ db/migrate/1_create_emails.rb
@@ -2,6 +2,7 @@ class CreateEmails < ActiveRecord::Migration[5.2]
   def change
     create_table :emails do |t|
       t.json 'mail'
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "emails", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.json "mail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
