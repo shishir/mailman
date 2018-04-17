e1df05d34508d914b923e248f1a1e7d1c09b8360
shishir <shishir.das@gmail.com>
2018-04-17 20:50:11 +1000

    Refactor

--- app/web/app.rb
+++ app/web/app.rb
@@ -2,6 +2,39 @@ require 'json'
 
 class App
   def call(env)
-    EmailsController.new(Rack::Request.new(env)).process
+    @request = Rack::Request.new(env)
+    if create?
+      return EmailsController.new(params).create
+    elsif status?
+      return EmailsController.new(params).status
+    elsif update?
+      return EmailsController.new(params).update
+    end
   end
+
+  private
+  def create?
+    @request.path_info =~ /send/ && @request.post?
+  end
+
+  def status?
+    @request.path_info =~ /mail\/[1-9]*\/status/ && @request.get?
+  end
+
+  def update?
+    false
+  end
+
+  def params
+    hsh = {}
+    if create?
+      hsh[:mail] = @request.body.read
+    end
+    if status?
+      match_data = /mail\/([1-9]*)\/status/.match(@request.path_info)
+      hsh[:id] = match_data[1]
+    end
+    hsh
+  end
+
 end
--- app/web/controllers/emails_controller.rb
+++ app/web/controllers/emails_controller.rb
@@ -1,32 +1,27 @@
 class EmailsController
-  attr_reader :request
+  attr_reader :params
 
-  def initialize(request)
-    @request = request
+  def initialize(params)
+    @params = params
   end
 
-  def send_path?
-    request.path_info =~ /send/
-  end
-
-  def status_path?
-    request.path_info =~ /mail\/([1-9]*)\/status/
-  end
-
-
-  def process
-    if send_path?
-      email = Email.new(mail: request.body.read, status: Email::SENDING)
-      if email.save
-        [201, {"Content-Type" => "application/json", "location" => "/mail/#{email.id}"}, [{id: email.id, links: [status: "/mail/#{email.id}/status", self: "/mail/#{email.id}"]}.to_json]]
-      else
-        [400, {"Content-Type" => "application/json"}, [{mail: {errors: email.errors}}.to_json] ]
-      end
-    elsif status_path?
-      if match_data = /mail\/([1-9]*)\/status/.match(request.path_info)
-        email = Email.find(match_data[1])
-        [200, {"Content-Type" => "application/json"}, [{id: email.id, status: email.status,  links: [self: "/mail/#{email.id}/status", show: "/mail/#{email.id}"]}.to_json]]
-     end
+  def create
+    email = Email.new(mail: params[:mail], status: Email::SENDING)
+    if email.save
+      [201, {"Content-Type" => "application/json", "location" => "/mail/#{email.id}"}, [{id: email.id, links: [status: "/mail/#{email.id}/status", self: "/mail/#{email.id}"]}.to_json]]
+    else
+      [400, {"Content-Type" => "application/json"}, [{mail: {errors: email.errors}}.to_json] ]
     end
+  end
+
+  def status
+    email = Email.find(params[:id])
+    [200, {"Content-Type" => "application/json"}, [{id: email.id, status: email.status,  links: [self: "/mail/#{email.id}/status", show: "/mail/#{email.id}"]}.to_json]]
+  end
+
+  def update
+    # match_data = /mail\/([1-9]*)\/status/.match(request.path_info)
+    # email = Email.find(match_data[1])
+    # [200, {"Content-Type" => "application/json"}, [{id: email.id, status: email.status,  links: [self: "/mail/#{email.id}/status", show: "/mail/#{email.id}"]}.to_json]]
   end
 end
