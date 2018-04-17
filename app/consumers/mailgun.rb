953eb82965919cd488836c63ea9d1e8bd96e4769
shishir <shishir.das@gmail.com>
2018-04-17 19:43:28 +1000

    Configure status queue

--- app/consumers/mailgun.rb
+++ app/consumers/mailgun.rb
@@ -1,10 +1,11 @@
 class Mailgun
   include Phobos::Handler
+  include Phobos::Producer
 
   def consume(payload, metadata)
     email = JSON.parse(payload)["mail"]
     Api::Mailgun.new.send(email)
   rescue Exception => e
-    p "foo"
+    self.producer.publish(MailmanConfig.status, payload, MailmanConfig.partition)
   end
 end
--- app/consumers/sendgrid.rb
+++ app/consumers/sendgrid.rb
@@ -5,6 +5,7 @@ class Sendgrid
     begin
       email = JSON.parse(payload)["mail"]
       Api::Sendgrid.new.send(email)
+      self.producer.publish(MailmanConfig.status, payload, MailmanConfig.partition)
     rescue Exception => e
       self.producer.publish(MailmanConfig.backup_topic, payload, MailmanConfig.partition)
     end
