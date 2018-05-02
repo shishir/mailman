d6dd63617fb1e1981d4680526e25f87e2f733ae9
shishir <shishir.das@gmail.com>
2018-05-03 09:08:33 +1000

    Remove kafka binaries

--- bin/kafka-start-server.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-./vendor/kafka_2.11-1.1.0/bin/kafka-server-start.sh config/server.properties
--- bin/zookeeper-start-server.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-./vendor/kafka_2.11-1.1.0/bin/zookeeper-server-start.sh config/zookeeper.properties
